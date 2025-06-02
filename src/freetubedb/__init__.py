d3547d4c19c483bbccfdb4517709f04bb3611236
FawkesOficial <mario.lourenco.morte@gmail.com>
2025-06-02 20:44:38 +0100

    updated project info files

---
+++ src/freetubedb/__init__.py
@@ -0,0 +1,14 @@
+# TODO
+# all = []
+
+__productname__ = "freetubedb"
+__version__ = "0.0.1"
+__description__ = "Python library for interacting with FreeTube's playlists.db file"
+__url__ = "https://github.com/FawkesOficial/python-freetubedb"
+__author__ = "FawkesOficial"
+__author_email__ = "mario.lourenco.morte@gmail.com"
+__license__ = "GNU General Public License v3 (GPLv3)"
+__bugtracker__ = "https://github.com/FawkesOficial/python-freetubedb/issues"
+__ci__ = "https://github.com/FawkesOficial/python-freetubedb/actions"
+__changelog__ = "https://github.com/FawkesOficial/python-freetubedb/releases"
+__cake__ = "lie"
